cae8a803ac0472fdafcb864d5fff2536cf53e28b
ZiedMB <bahri.med.zied@gmail.com>
2018-03-20 10:41:12 +0000

    return list of result, return breaking the loop

--- blkidfunc.py
+++ blkidfunc.py
@@ -4,11 +4,13 @@ import commands
 import os
 
 def blkid():
+  result = []
   out = commands.getoutput('sudo blkid | grep -v sdb')
   lines = out.splitlines()
   for line in lines:
      if 'dev/sd' in line:
          index = line.rfind('=', 0, 40)
          param = line[index + 2:53]
-  return param
-print (blkid())
+     result.append(param)
+  return result
+#print(blkid())
